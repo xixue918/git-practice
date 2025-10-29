31275fec951a072e8936ec3932e93555b8f187f7
xixue918 <2811395908@qq.com>
2025-10-29 20:22:15 +0800

    初始化项目：添加主程序文件Main.java（实现简单问候功能）

---
+++ Main.java
@@ -0,0 +1,12 @@
+// Main.java
+public class Main {
+    // 定义问候方法
+    public static void greet() {
+        System.out.println("Hello, Git!");
+    }
+
+    // 主方法（程序入口）
+    public static void main(String[] args) {
+        greet(); // 调用问候方法
+    }
+}
