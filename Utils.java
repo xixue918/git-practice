7f71c583209158c9176035ec805a67e3643643b9
xixue918 <2811395908@qq.com>
2025-10-29 20:24:09 +0800

    新增工具类Utils.java：实现整数加法和乘法功能

---
+++ Utils.java
@@ -0,0 +1,12 @@
+// Utils.java
+public class Utils {
+    // 加法功能
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    // 乘法功能
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
